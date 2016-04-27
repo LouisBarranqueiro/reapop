fee6d16bde13a6ba6001f2070dbeca4129be6b39
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-04-27 10:59:28 +0200

    add 4 tests for notification reducers

--- test/redux/notifications.js
+++ test/redux/notifications.js
@@ -1,19 +1,67 @@
 import {expect} from 'chai';
-import {types, actions} from '../../src/redux/modules/notifications';
+import reducer, {types, actions} from '../../src/redux/modules/notifications';
+
+const notification = {
+  id: 1234567899874,
+  message: 'message',
+  type: 'error',
+  dismissible: false,
+  dismissAfter: 4000
+};
 
 describe('actions', () => {
   it('should create an action to add a notification', () => {
-    const notification = {
-      id: 1234567899874,
-      message: 'message',
-      type: 'error',
-      dismissible: false,
-      dismissAfter: 4000
-    };
     const expectedAction = {
-      type: types.NOTIFICATION_ADD,
+      type: types.ADD_NOTIFICATION,
       payload: notification
     };
     expect(actions.pushNotification(notification)).to.deep.equal(expectedAction)
+  });
+});
+
+describe('reducers', () => {
+  it('should return the initial state', () => {
+    const expectedAction = [];
+    expect(reducer(undefined, {})).to.deep.equal(expectedAction)
+  });
+  
+  it('should handle ADD_NOTIFICATION', () => {
+    expect(
+      reducer([], {
+        type: types.ADD_NOTIFICATION,
+        payload: notification
+      })
+    ).to.deep.equal(
+      [notification]
+    )
+  });
+
+  it('should handle UPDATE_NOTIFICATION', () => {
+    const newNotification = {
+      id: notification.id,
+      message: 'message updated',
+      type:'error',
+      dismissible:true,
+      dismissAfter: 10000
+    };
+    expect(
+      reducer([notification],{
+        type: types.UPDATE_NOTIFICATION,
+        payload: newNotification
+      })
+    ).to.deep.equal(
+      [newNotification]
+    )
+  });
+
+  it('should handle REMOVE_NOTIFICATION', () => {
+    expect(
+      reducer([notification],{
+        type: types.REMOVE_NOTIFICATION,
+        payload: notification.id
+      })
+    ).to.deep.equal(
+      []
+    )
   });
 });
