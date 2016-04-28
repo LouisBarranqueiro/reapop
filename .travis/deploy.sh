91424ba829133b8550e5b1913a053cb33913d499
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-04-28 12:07:03 +0200

    add deploy script to deploy demo on github pages

---
+++ .travis/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd dist
+
+git init
+git config user.name "Travis Bot"
+git config user.email $USER_EMAIL
+
+git remote add upstream "https://$GH_TOKEN@github.com/LouisBarranqueiro/react-redux-notification.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
