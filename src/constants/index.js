44eb6354f4e25e26321fb1e489ab5fc7efe11802
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-04-29 12:42:41 +0200

    add constants file to store notification status

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+export const INFO_STATUS = "info";
+export const SUCCESS_STATUS = "success";
+export const WARNING_STATUS = "warning";
+export const ERROR_STATUS = "error";
+
+const NOTIFICATION_STATUS = {
+  info: INFO_STATUS,
+  success: SUCCESS_STATUS,
+  warning: WARNING_STATUS,
+  error: ERROR_STATUS
+};
+
+export default NOTIFICATION_STATUS;
