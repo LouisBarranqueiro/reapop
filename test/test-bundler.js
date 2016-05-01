9ffa4b0ce905e965d6f981175ab36590f2e6ed89
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-05-01 04:07:23 +0200

    define a test bundler

---
+++ test/test-bundler.js
@@ -0,0 +1,11 @@
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+// This will emulate a full ES2015 (ES6) environment
+import 'babel-polyfill';
+
+expect.extend(expectJSX);
+global.expect = expect;
+
+
+var context = require.context('./', true, /index\.js/);
+context.keys().forEach(context);
