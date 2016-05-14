20e2236b4dfc2a71e28734e2b791f00dc51f375d
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-05-14 18:39:55 +0200

    update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,12 +4,22 @@ export const SUCCESS_STATUS = 'success';
 export const WARNING_STATUS = 'warning';
 export const ERROR_STATUS = 'error';
 
-const NOTIFICATION_STATUS = {
-  default: DEFAULT_STATUS,
-  info: INFO_STATUS,
-  success: SUCCESS_STATUS,
-  warning: WARNING_STATUS,
-  error: ERROR_STATUS
-};
+export const STATUS = [
+  DEFAULT_STATUS,
+  INFO_STATUS,
+  SUCCESS_STATUS,
+  WARNING_STATUS,
+  ERROR_STATUS
+];
 
-export default NOTIFICATION_STATUS;
+export const TOP_LEFT_POSITON = 'tl';
+export const TOP_RIGHT_POSITON = 'tr';
+export const BOTTOM_LEFT_POSITON = 'bl';
+export const BOTTOM_RIGHT_POSITON = 'br';
+
+export const POSITIONS = [
+  TOP_LEFT_POSITON,
+  TOP_RIGHT_POSITON,
+  BOTTOM_LEFT_POSITON,
+  BOTTOM_RIGHT_POSITON
+];
