39ad72b2eb251a22f889e4d9ce215e1e05b6f0fc
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-05-15 18:08:37 +0200

    update demo structure

--- demo/src/components/Sidebar/index.js
+++ demo/src/components/Sidebar/index.js
@@ -1,4 +1,23 @@
-import Sidebar from './Sidebar';
-import css from './Sidebar.scss';
-export {css};
+import React, {Component} from 'react';
+import css from './styles.scss';
+import NotificationCreator from '../NotificationCreator';
+import NotificationExamples from '../NotificationExamples';
+
+class Sidebar extends Component {
+  /**
+   * Render component
+   * @returns {XML}
+   * @private
+   */
+  render() {
+    return (
+      <div className={css['sidebar']}>
+        <NotificationCreator/>
+        <hr/>
+        <NotificationExamples/>
+      </div>
+    );
+  }
+}
+
 export default Sidebar;
