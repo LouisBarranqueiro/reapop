ee39950b5640dbbc7f9bd627900866cd9eadc5f9
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-05-15 17:30:52 +0200

    move welcome notification in Demo component

--- demo/src/components/Demo/Demo.js
+++ demo/src/components/Demo/Demo.js
@@ -1,11 +1,73 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import Sidebar from '../Sidebar';
 import Footer from '../Footer';
 import RandomNotificationCreator from '../RandomNotificationCreator';
-import Notifications from '../../../../src/index';
+import NotificationsSystem, {addNotification as notify, updateNotification} from '../../../../src';
 import {css} from './index';
 
 class Demo extends Component {
+  static propTypes = {
+    notify: React.PropTypes.func.isRequired,
+    updateNotification: React.PropTypes.func.isRequired
+  };
+
+  /**
+   * Bind methods
+   * @param {Object} props
+   * @returns {void}
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: window.innerWidth
+    };
+    this._updateWindowWidth = this._updateWindowWidth.bind(this);
+  }
+
+  /**
+   * Update window width state
+   * @returns {void}
+   * @private
+   */
+  _updateWindowWidth() {
+    this.setState({windowWidth: window.innerWidth});
+  }
+
+  /**
+   * Update window height state when component is mounted
+   * @returns {void}
+   */
+  componentDidMount() {
+    const {notify, updateNotification} = this.props;
+
+    window.addEventListener('resize', this._updateWindowWidth);
+    let notif = notify({
+      title: 'Welcome on demo!',
+      message: 'Hey buddy, here you can see what you can do with it.',
+      status: 'info',
+      dismissible: false,
+      dismissAfter: 0
+    });
+    setTimeout(function() {
+      notif.message = 'If you got any questions, create an issue on Github repository.';
+      notif.dismissAfter = 5000;
+      updateNotification(notif);
+    }, 4000);
+  }
+
+  /**
+   * Update window height state when component will unmount
+   * @returns {void}
+   */
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._updateWindowWidth);
+  }
+
+  /**
+   * Render component
+   * @returns {XML}
+   */
   render() {
     const defaultValues = {
       status: 'info',
@@ -14,6 +76,7 @@ class Demo extends Component {
       dismissAfter: 5000,
       allowHTML: true
     };
+
     return (
       <div className={css['background']}>
         <div className={`${css['logo-container']} text-center`}>
@@ -21,12 +84,12 @@ class Demo extends Component {
           <div className={css['description']}>A React and Redux notifications system</div>
           <RandomNotificationCreator/>
         </div>
-        <Notifications defaultValues={defaultValues}/>
-        <Sidebar/>
+        <NotificationsSystem defaultValues={defaultValues}/>
+        {(window.innerWidth > 767 ? <Sidebar/> : '')}
         <Footer/>
       </div>
     );
   }
 }
 
-export default Demo;
+export default connect(null, {notify, updateNotification})(Demo);
--- demo/src/components/Sidebar/Sidebar.js
+++ demo/src/components/Sidebar/Sidebar.js
@@ -1,35 +1,9 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {
-  addNotification as notify,
-  updateNotification
-} from '../../../../src/index';
 import {css} from './index';
 import NotificationCreator from '../NotificationCreator';
 import NotificationExamples from '../NotificationExamples';
 
 class Sidebar extends Component {
-  static propTypes = {
-    notify: React.PropTypes.func.isRequired,
-    updateNotification: React.PropTypes.func.isRequired
-  };
-  
-  componentDidMount() {
-    const {notify, updateNotification} = this.props;
-    let notif = notify({
-      title: 'Welcome on demo!',
-      message: 'Hey buddy, here you can see what you can do with it.',
-      status: 'info',
-      dismissible: false,
-      dismissAfter: 0
-    });
-    setTimeout(function() {
-      notif.message = 'If you got any questions, create an issue on Github repository.';
-      notif.dismissAfter = 5000;
-      updateNotification(notif);
-    }, 4000);
-  }
-
   /**
    * Render component
    * @returns {XML}
@@ -46,4 +20,4 @@ class Sidebar extends Component {
   }
 }
 
-export default connect(null, {notify, updateNotification})(Sidebar);
+export default Sidebar;
