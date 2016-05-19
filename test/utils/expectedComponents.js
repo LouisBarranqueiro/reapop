1597815129c333d74edc0a17a61d21ebdfed89ed
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-05-19 21:04:18 +0200

    fix test for NotificationsSystem component

--- test/utils/expectedComponents.js
+++ test/utils/expectedComponents.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 import TransitionGroup from 'react/lib/ReactCSSTransitionGroup';
 import {POSITIONS} from '../../src/constants';
 import Notification from '../../src/components/Notification';
+import NotificationsContainer from '../../src/components/NotificationsContainer';
+import {defaultValues} from '../../src/components/NotificationsSystem';
 
 export class ExpectedNotification extends Component {
   /**
@@ -143,6 +146,70 @@ export class ExpectedNotificationsContainer extends Component {
           transitionLeaveTimeout={leaveTimeout}>
           {this._renderNotifications()}
         </TransitionGroup>
+      </div>
+    );
+  }
+}
+
+export class ExpectedNotificationsSystem extends Component {
+  // default properties
+  static defaultProps = {
+    notifications: [],
+    defaultValues
+  };
+
+  constructor(props) {
+    super(props);
+    this._renderNotificationsContainers = this._renderNotificationsContainers.bind(this);
+  }
+
+  /**
+   * Render notifications containers
+   * @returns {XML}
+   * @private
+   */
+  _renderNotificationsContainers() {
+    const {notifications, defaultValues: {position}, theme} = this.props;
+    // render all notifications in the same container at the top for small screens
+    if (window.innerWidth < theme.smallScreenMin) {
+      return (
+        <NotificationsContainer key='top' position='top' defaultValues={defaultValues}
+          theme={theme} notifications={notifications}/>
+      );
+    }
+    let positions = _.values(POSITIONS);
+    // extract the default position of all positions
+    positions.splice(positions.indexOf(position), 1);
+    let notifs = notifications.filter((notif) => {
+      return (!notif.position || notif.position === position);
+    });
+    // init array with all notifications with default position
+    let JSX = [
+      <NotificationsContainer key={position} position={position} defaultValues={defaultValues}
+        theme={theme} notifications={notifs}/>
+    ];
+    // fill array with others containers
+    JSX = JSX.concat(positions.map((position) => {
+      let notifs = notifications.filter((notif) => {
+        return position === notif.position;
+      });
+      return (
+        <NotificationsContainer key={position} position={position} defaultValues={defaultValues}
+          theme={theme} notifications={notifs}/>
+      );
+    }));
+    return JSX;
+  }
+
+  /**
+   * Render
+   * @returns {XML}
+   */
+  render() {
+    const {className} = this.props.theme.notificationsSystem;
+    return (
+      <div className={className}>
+        {this._renderNotificationsContainers()}
       </div>
     );
   }
