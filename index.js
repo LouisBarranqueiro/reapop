6e9cc3d3a8f587e4d383d0e127a1ec58847c55a0
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-05-20 19:16:47 +0200

    update entrypoint index.js

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-/*
- The MIT License (MIT)
-
- Copyright (c) 2016 Louis Barranqueiro
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
- */
 import NotificationsSystem from './src/components/NotificationsSystem';
 import {STATUS, POSITIONS} from './src/constants';
 import reducer, {
@@ -33,17 +10,20 @@ import reducer, {
 
 export {
   // constants
+  // all available status
   STATUS,
+  // all available positions
   POSITIONS,
-  // actions
+  // all action creators
   actions,
   // actions types
   types,
   // reducer
   reducer,
-  // all actions separately
+  // action creators separately
   addNotification,
   updateNotification,
   removeNotification
 };
+// NotificationsSystem React component
 export default NotificationsSystem;
