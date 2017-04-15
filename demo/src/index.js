bc7823d4c0dd8b4c91b0094bcee7c91557d99b2d
Bobby Dixit <bobbydxt8may@gmail.com>
2017-04-16 02:41:45 +0530

    thunk middleware removed

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {render} from 'react-dom';
-import {createStore, compose, combineReducers, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
+import {createStore, compose, combineReducers} from 'redux';
 import App from './components/Demo';
 import {reducer as notificationsReducer} from '../../src';
 import 'babel-polyfill';
@@ -10,7 +9,6 @@ import './styles/style.scss';
 
 // store
 const createStoreWithMiddleware = compose(
-  applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f // add support for Redux dev tools
 )(createStore);
 const store = createStoreWithMiddleware(combineReducers({
