d11ffd1dca4a8d08b23a7bdd8d5f68e251e6f533
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2018-01-01 19:54:21 +0100

    use react-addons-update

--- demo/src/components/NotificationCreator/index.js
+++ demo/src/components/NotificationCreator/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import update from 'react/lib/update';
+import update from 'react-addons-update';
 import {connect} from 'react-redux';
 import Switch from '../Switch';
 import {addNotification as notify} from '../../../../src/index';
