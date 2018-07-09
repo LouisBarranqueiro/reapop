c3fedb31b1288805d7c42dfade22489727d6cd3e
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2018-07-08 21:19:28 -0700

    increase timeout

--- test/store/notifications.test.js
+++ test/store/notifications.test.js
@@ -79,7 +79,7 @@ describe('notifications', () => {
           // image should be loaded now, so store should contains the notification updated
           expect(store.getActions()).toEqual(expectedAction);
           done();
-        }, 1000);
+        }, 3000);
       });
 
       it('should create an action to add a notification ' +
@@ -202,13 +202,13 @@ describe('notifications', () => {
           // image should be loaded now, so store should contains the notification updated
           expect(store.getActions()).toEqual(expectedAction);
           done();
-        }, 500);
+        }, 3000);
       });
 
       it('should load image then create an action to update a notification (image is different)', (done) => {
         // update notification image url
         const notificationUpdated = Object.assign({}, notification, {
-          image: 'https://avatars3.githubusercontent.com/u/4705626?v=3&s=2'
+          image: imageUrl
         });
         const expectedAction = [{
           type: types.UPDATE_NOTIFICATION,
@@ -224,7 +224,7 @@ describe('notifications', () => {
           // image should be loaded now, so store should contains the notification updated
           expect(store.getActions()).toEqual(expectedAction);
           done();
-        }, 500);
+        }, 3000);
       });
     });
 
