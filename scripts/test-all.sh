8cec674cd797d6b54545314a0e5ab803bb3580e8
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2019-06-15 15:59:44 -0700

    add latest peer deps as dev deps

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -1,15 +1,4 @@
-npm i react@0.14 react-dom@0.14 react-addons-test-utils@0.14 redux@2 react-redux@2 &&
 npm t &&
-npm uninstall redux react-redux &&
-
-npm i redux@3 react-redux@3.1 &&
-npm t &&
-npm uninstall react react-dom react-addons-test-utils redux react-redux &&
-
-npm i react@15 react-dom@15 react-addons-test-utils@15 redux@4 react-redux@4 &&
-npm t &&
-npm uninstall react react-dom react-addons-test-utils redux react-redux &&
-
-npm i react@16.5.2 react-dom@16 redux@4 react-redux@5.0.6 &&
-npm t &&
-npm uninstall react react-dom react-addons-test-utils redux react-redux
+npm i react@15 react-dom@15 react-addons-test-utils@15 redux@4 react-redux@5.0.6 && npm t &&
+npm i react@0.14 react-dom@0.14 react-addons-test-utils@0.14 redux@3 react-redux@4 && npm t &&
+npm i react@0.14 react-dom@0.14 react-addons-test-utils@0.14 redux@2 react-redux@3.1 && npm t
