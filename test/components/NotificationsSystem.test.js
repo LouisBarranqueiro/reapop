9bc35791bb37e5467265e2ee34b399adec16a048
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2019-06-15 18:49:24 -0700

    update NotificationSystem tests

--- test/components/NotificationsSystem.test.js
+++ test/components/NotificationsSystem.test.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import {Provider} from 'react-redux';
 import ConnectedNotificationsSystem, {
   NotificationsSystem
 } from '../../src/components/NotificationsSystem';
 import Notification from '../../src/components/Notification';
-import NotificationsContainer from '../../src/components/NotificationsContainer';
 import {genNotification, genNotifications, mockStore, checkPropTypes} from '../utils/fixtures';
-import {ExpectedNotificationsSystem} from '../utils/expectedComponents';
 import theme from 'reapop-theme-wybo';
 
 describe('<NotificationsSystem/>', () => {
-  const otherProps = {
-    theme
-  };
-
   it('should validate props', () => {
     const errors = checkPropTypes({
       notifications: [],
@@ -32,77 +26,30 @@ describe('<NotificationsSystem/>', () => {
   });
 
   it('should mount with default props', () => {
-    const props = mount(<NotificationsSystem {...otherProps}/>).props();
+    const props = mount(<NotificationsSystem theme={theme}/>).props();
     expect(props.notifications).toEqual([]);
   });
 
-  it('should render 1 empty notifications container (mobile)', () => {
-    const wrapper = mount(<NotificationsSystem {...otherProps}/>);
-    const expectedWrapper = mount(<ExpectedNotificationsSystem {...otherProps}/>);
-    expect(wrapper.find(NotificationsContainer).length).toEqual(1);
-    expect(wrapper.find(Notification).length).toEqual(0);
-    expect(wrapper.html()).toEqual(expectedWrapper.html());
+  it('should render 1 notification containers (mobile)', () => {
+    const wrapper = shallow(<NotificationsSystem theme={theme}/>);
+    wrapper.setState({windowWidth: theme.smallScreenMin - 1});
+    expect(wrapper.debug({verbose: true})).toMatchSnapshot();
   });
 
-  it('should render 8 empty notifications containers (desktop)', () => {
-    // define the small screen breakpoint under default window width of PhantomJS (400px)
-    // to simulate a desktop screen and test a portion of code
-    const customTheme = Object.assign({}, theme, {smallScreenMin: 380});
-    const wrapper = mount(<NotificationsSystem theme={customTheme}/>);
-    const expectedWrapper = mount(<ExpectedNotificationsSystem theme={customTheme}/>);
-    expect(wrapper.find(NotificationsContainer).length).toEqual(8);
-    expect(wrapper.find(Notification).length).toEqual(0);
-    expect(wrapper.html()).toEqual(expectedWrapper.html());
-  });
-
-  it('should render notifications in 1 notifications containers (mobile)', () => {
-    const notifications = genNotifications(3);
-    const store = mockStore({notifications});
-    const wrapper = mount(
-      <Provider store={store}>
-        <ConnectedNotificationsSystem notifications={notifications} {...otherProps}/>
-      </Provider>
-    );
-    const expectedWrapper = mount(
-      <Provider store={store}>
-        <ExpectedNotificationsSystem notifications={notifications} {...otherProps}/>
-      </Provider>
-    );
-    expect(wrapper.find(NotificationsContainer).length).toEqual(1);
-    expect(wrapper.find(Notification).length).toEqual(3);
-    expect(wrapper.html()).toEqual(expectedWrapper.html());
-  });
-
-  it('should render notifications in 8 notifications containers (desktop)', () => {
-    const notifications = genNotifications(3);
-    const store = mockStore({notifications});
-    // define the small screen breakpoint under default window width of PhantomJS (400px)
-    // to simulate a desktop screen and test a portion of code
-    const customTheme = Object.assign({}, theme, {smallScreenMin: 380});
-    const wrapper = mount(
-      <Provider store={store}>
-        <ConnectedNotificationsSystem notifications={notifications} theme={customTheme}/>
-      </Provider>
-    );
-    const expectedWrapper = mount(
-      <Provider store={store}>
-        <ExpectedNotificationsSystem notifications={notifications} theme={customTheme}/>
-      </Provider>
-    );
-    expect(wrapper.find(NotificationsContainer).length).toEqual(8);
-    expect(wrapper.find(Notification).length).toEqual(3);
-    expect(wrapper.html()).toEqual(expectedWrapper.html());
+  it('should render 8 notification containers (desktop)', () => {
+    const wrapper = shallow(<NotificationsSystem theme={theme}/>);
+    wrapper.setState({windowWidth: theme.smallScreenMin});
+    expect(wrapper.debug({verbose: true})).toMatchSnapshot();
   });
 
   it('should use filter and render 1 notification', () => {
     const notifications = genNotifications(3);
     notifications.push(genNotification({style: 'alert'}));
     const store = mockStore({notifications});
-    const customTheme = Object.assign({}, theme, {smallScreenMin: 380});
     const wrapper = mount(
       <Provider store={store}>
         <ConnectedNotificationsSystem
-          theme={customTheme}
+          theme={theme}
           filter={notif => notif.style === 'alert'}
         />
       </Provider>
