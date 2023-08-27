49be2934c9521f0200f0b233da227d0b21098d66
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2023-08-26 19:08:14 -0700

    chore: lint code

--- src/contexts/reapopNotificationsContext.ts
+++ src/contexts/reapopNotificationsContext.ts
@@ -1,14 +1,10 @@
 import {createContext} from 'react'
 import {Notification, Status} from '../reducers/notifications/types'
 
-export declare function upsertNotification(notification: Partial<Notification>): Notification
-export declare function upsertNotification(message: string, options?: Partial<Notification>): Notification
-export declare function upsertNotification(
-    message: string,
-    status: Status,
-    options?: Partial<Notification>
-): Notification
-export declare function upsertNotification(
+declare function upsertNotification(notification: Partial<Notification>): Notification
+declare function upsertNotification(message: string, options?: Partial<Notification>): Notification
+declare function upsertNotification(message: string, status: Status, options?: Partial<Notification>): Notification
+declare function upsertNotification(
     ...args: [Partial<Notification> | string, (Partial<Notification> | Status)?, Partial<Notification>?]
 ): Notification
 
